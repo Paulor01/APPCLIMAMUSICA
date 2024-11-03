694e9cd82501a6a09309a6b82b503e9792317554
Paulo Rodrigues <115431685+Paulor01@users.noreply.github.com>
2024-11-03 20:35:23 -0300

    iniciando javascripit

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function botaoDeBusca() {
+    const input = document.getElementById('input-busca').value;
+
+  const input  = document.getElementById ('input-busca');
+
+  console.log(input)
+
+}
+
+
+
+
+
