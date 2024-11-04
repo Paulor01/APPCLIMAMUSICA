388e78b47d87e9d91e791a38156ff473e962c6f3
Paulo Rodrigues <115431685+Paulor01@users.noreply.github.com>
2024-11-04 00:05:33 -0300

    Falta trocar as imagens do tempo o comando SRC/assets/ não deu certo

--- scripts.js
+++ scripts.js
@@ -28,7 +28,8 @@ function abrirInput(){
 
 function movimentoInput(inputValue){
    const visibility =  document.getElementById('input-busca').style.visibility;
-   console.log(inputValue)
+   
+   inputValue && procurarCidade(inputValue);
 
    visibility === 'hidden' ? abrirInput : fecharInput ();
 }
@@ -45,13 +46,37 @@ document.addEventListener('DOMContentLoad',() => {
 })
 
 
-async function procurarCidade() {
-    const dados = await fetch (`https://api.openweathermap.org/data/2.5/weather?q=barcelona&appid=${apiKey}`)
-    const resultado = await dados.json()
-    console.log(dados, '<<')
+async function procurarCidade(city) {
+    try {
+        const dados = await fetch (`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=pt_br`)
+        
+         if(dados.status ===200){
+            const resultado = await dados.json();
 
+            mostrarClimaNaTela(resultado);
+            console.log(resultado, '<<')
+
+         }else{
+            throw new Error
+         }     
+
+        
+
+    } catch  {
+        alert ('A pesuisa por cidade deu errado!');
+        
+    }    
+    
+}
+
+function mostrarClimaNaTela(resultado){
+    
+    document.querySelector('.nome-cidade').innerHTML = `${resultado.name}`;
+    document.querySelector('.temperatura').innerHTML = `${resultado.main.temp. toFixed(0)}°C`;
+    document.querySelector('.maxTemperatura').innerHTML = `máx: ${resultado.main.temp_max.toFixed(0)}°C`;
+    document.querySelector('.minTemperatura').innerHTML = `mín: ${resultado.main.temp_min.toFixed(0)}°C`;
 
 }
 
-procurarCidade()
+
 
